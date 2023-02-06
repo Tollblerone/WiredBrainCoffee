9ad7e73ff3adb139635ba93af6e5314086d597ea
Gundam <Gundam@DESKTOP-Q0M3IVS>
2023-02-06 12:43:05 +1100

    Add project files.

---
+++ WiredBrainCoffeeSurveys.Reports/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace WiredBrainCoffeeSurveys.Reports
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var tasks = new List<string>();
+
+            //calculated values
+            double responseRate = Q1Results.NumberResponded / Q1Results.NumberSurveyed;
+            double overallScore = (Q1Results.ServiceScore + Q1Results.CoffeeScore + Q1Results.FoodScore + Q1Results.PriceScore) / 4;
+
+            if (Q1Results.CoffeeScore < Q1Results.FoodScore)
+            {
+                tasks.Add("Investigate coffee recipes and ingedients.");
+            }
+
+            if (overallScore > 8.0)
+            {
+                tasks.Add("Work With Leadership to reward staff");
+            }
+            else
+            {
+                tasks.Add("work with employees for improvement ideas.");
+            }
+
+            if (responseRate < .33)
+            {
+                tasks.Add("Research options to improve response rate.");
+            }
+            else if (responseRate > .33 && responseRate < .66)
+            {
+                tasks.Add("Reward participants with free coffee coupon.");
+            }
+            else
+            {
+                tasks.Add("Rewards participants with discount coffee coupon.");
+            }
+        }
+    }
+}
+                                                                                                                                                                                    
